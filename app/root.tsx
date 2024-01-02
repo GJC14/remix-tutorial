e67474b114465180dac38f092da8ed78ddb996af
Gabriel <i.myself.am.joe@gmail.com>
2024-01-03 03:28:50 +0800

    Deleting record finished

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,40 @@
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css";
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+import { getContacts, createEmptyContact } from "./data";
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+
   return (
     <html lang="en">
       <head>
@@ -35,15 +62,50 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? (
+                          <span>★</span>
+                        ) : null}
+                      </Link>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+        <div 
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+          id="detail"
+        >
+          <Outlet />
         </div>
 
         <ScrollRestoration />
